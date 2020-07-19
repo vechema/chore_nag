294fc41bd66aa574b6ddb2da928e00837dccdda6
Jo Egner <vechema@gmail.com>
2020-07-19 16:12:49 -0500

    Forget clap, use structopt

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use clap::{load_yaml, App};
-use std::env;
+use structopt::StructOpt;
 mod other;
 
+#[derive(StructOpt, Debug)]
+#[structopt(about = "the stupid content tracker")]
+enum ChoreNag {
+  #[structopt(about = "Show all chores")]
+  List {},
+}
+
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  println!("{:?}", args);
-
-  other::help();
-
-  let yaml = load_yaml!("cli.yml");
-  let matches = App::from_yaml(yaml).get_matches();
+  let opt = ChoreNag::from_args();
 
   let chores = vec!["clean sink", "clean up after dog"];
+
+  match opt {
+    ChoreNag::List {} => println!("{:?}", chores),
+  }
 }
